18514ee1b0dab930badd413e0fc549a289d568a0
Akshaya Arigela <akshayaarigela@Akshayas-MacBook-Air.local>
2026-01-29 19:46:33 +0530

    Prepare for Render: Add Dockerfile and update db.php

--- db.php
+++ db.php
@@ -1,10 +1,12 @@
 <?php
-$host = 'localhost';
-$user = 'root';
-$password = '';
-$dbname = 'bookstore';
-$conn = new mysqli($host, $user, $password, $dbname);
+$servername = getenv("DB_HOST");
+$username = getenv("DB_USER");
+$password = getenv("DB_PASSWORD");
+$dbname = getenv("DB_NAME");
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
 if ($conn->connect_error) {
-    die('Connection failed: ' . $conn->connect_error);
+    die("Connection failed: " . $conn->connect_error);
 }
 ?>
